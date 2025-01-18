cdc99bff3950e150835d6ebf7aa3965a9bd0c438
Jacob Schreuder <jacobdschreuder@gmail.com>
2025-01-18 23:09:39 +1100

    fixes for ansible

--- config_management.sh
+++ config_management.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+# run base git sync
 ./setup.sh
+
+# ensure ansible package is present
 sudo pacman -S ansible --noconfirm
+
+# ensure the collection is locally installed
 ansible-galaxy collection install jacobvhs.ansible_roles_arch --force
+
+# run configuration management
 ansible-playbook arch-configuration-management.yml
